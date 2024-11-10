8fc84b5e8081dc32543b5711eaa676831fb25a2b
WarriorMind04 <josemiguel.guerrerojim@gmail.com>
2024-11-10 11:37:41 -0600

    Cambios Viejon

--- app/src/main/java/com/example/papalote/ui/theme/Theme.kt
+++ app/src/main/java/com/example/papalote/ui/theme/Theme.kt
@@ -10,6 +10,15 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.papalote.R
+
+// Definición de la familia de fuentes para Aperçu Pro Black
+val ApercuProBlackFontFamily = FontFamily(
+    Font(R.font.apercu_pro_black, FontWeight.Black)
+)
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
